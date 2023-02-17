fe47b23460165ba2ae9726df5615cc1436bd345c
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-17 14:13:10 -0300

    Esqueleto de las funciones para Sistema de mensajes

--- ejercitacion_punteros/src/SistemaDeMensajes.cpp
+++ ejercitacion_punteros/src/SistemaDeMensajes.cpp
@@ -1,3 +1,21 @@
 #include "SistemaDeMensajes.h"
 
-// Completar...
+/* Completar */
+
+SistemaDeMensajes::SistemaDeMensajes() {}
+
+void SistemaDeMensajes::registrarJugador(int id, string ip){
+    // Pre: 0 <= id < 4
+}
+
+bool SistemaDeMensajes::registrado(int id) const{
+
+}
+
+void SistemaDeMensajes::enviarMensaje(int id, string mensaje){
+    // Pre: registrado(id)
+}
+
+string SistemaDeMensajes::ipJugador(int id) const{
+    // Pre: registrado(id)
+}
